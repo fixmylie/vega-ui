513f4e2673ed4f7f9cf4878afb7623eb68ece8c1
Davs107 <davs777t@gmail.com>
2020-06-30 18:30:32 +0300

    test(radio-list): тесты
    Jira issue: VEGA-159

--- packages/components/radio-list/src/RadioList.test.tsx
+++ packages/components/radio-list/src/RadioList.test.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
 
 import { RadioList, RadioListProps } from './RadioList';
+import { RadioListItemProps } from './RadioListItem';
 
-function renderComponent(props?: RadioListProps): RenderResult {
-  return render(<RadioList {...props} />);
-}
+type ComponentsProps = {
+  radioListProps?: Partial<RadioListProps>;
+  itemProps?: Partial<RadioListItemProps>;
+};
+
+const renderComponent = (componentsProps: ComponentsProps = {}): RenderResult => {
+  const { radioListProps, itemProps } = componentsProps;
+  const props = {
+    radioListProps,
+    itemProps,
+  };
+
+  return render(
+    <RadioList {...props.radioListProps} data-testid="RadioList">
+      <RadioList.Item isActive {...props.itemProps} data-testid="RadioList:Item">
+        TEST
+      </RadioList.Item>
+      <RadioList.Item>TEST2</RadioList.Item>
+    </RadioList>,
+  );
+};
+
+const findItem = (testId = 'RadioList:Item'): Element => {
+  return screen.getByTestId(testId);
+};
 
 describe('RadioList', () => {
   test('рендерится без ошибок', () => {
-    renderComponent({ title: 'Test' });
+    renderComponent();
+  });
+});
+
+describe('RadioListItem', () => {
+  test('проставляется класс is-active', () => {
+    renderComponent();
+
+    const item = findItem();
+
+    expect(item.classList.contains('is-active')).toBe(true);
   });
 });
