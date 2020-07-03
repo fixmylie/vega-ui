7d31e9013d93354da8276fddd93f3c7dc8ccf070
hitmanet <nathan200033@gmail.com>
2020-07-03 14:43:04 +0300

    fix(layout): правки в рендере порталов
    Jira issue: VEGA-126

--- packages/hooks/src/use-mount/use-mount.ts
+++ packages/hooks/src/use-mount/use-mount.ts
@@ -1,19 +1,18 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export function useMount(fn: Function = (): void => {}): React.RefObject<boolean> {
-  const isMountedRef = useRef(false);
+  const [isMounted, setIsMounted] = useState(false);
   const fnRef = useRef(fn);
 
   useEffect(() => {
     const result = fnRef.current();
-    isMountedRef.current = true;
+    setIsMounted(true);
 
     return (): void => {
       if (typeof result === 'function') {
         result();
       }
-      isMountedRef.current = false;
     };
   }, []);
-  return isMountedRef;
+  return isMounted;
 }
